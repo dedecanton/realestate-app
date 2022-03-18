43c124949a7a82b577067d0f1813ab294976e897
dedecanton <andrecanton18@gmail.com>
2022-03-18 10:48:10 -0300

    progress ui

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,29 @@ import nProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
+import nprogress from "nprogress";
 
 function MyApp({ Component, pageProps }) {
+  nprogress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    nProgress.start();
+  });
+  Router.events.on("routeChangeComplete", () => {
+    nProgress.done();
+  });
+
   return (
     <>
-      <Head></Head>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
